b2536860234f133b18dac82eec8ccfa27619bffb
DigitalNaut <johnbernalfsd@gmail.com>
2021-07-27 09:08:29 -0500

    Revampted Gallery
    + Added reusable GridProfile component
    + Formatted grid
    + Restructured some CSS to accommodate changes

---
+++ src/components/GridProfile/grid-profile.mjs
@@ -0,0 +1,17 @@
+import { getHtml } from "/src/js/moduleLoader.mjs";
+
+const template = document.createElement("template");
+template.innerHTML = await getHtml("../src/components/GridProfile/grid-profile.htm");
+
+class GridProfile extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define("grid-profile", GridProfile);
