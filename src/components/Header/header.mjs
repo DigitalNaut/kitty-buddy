53e43d8613743857905e3585369d59163e9e662f
DigitalNaut <johnbernalfsd@gmail.com>
2021-07-27 22:06:11 -0500

    Formatting
    Minor fixes

--- src/components/Header/header.mjs
+++ src/components/Header/header.mjs
@@ -1,7 +1,7 @@
 import { getHtml } from "/src/js/moduleLoader.mjs";
 
 const template = document.createElement("template");
-template.innerHTML = await getHtml("../src/components/Header/header.htm");
+template.innerHTML = await getHtml("/src/components/Header/header.htm");
 
 class Header extends HTMLElement {
   constructor() {
