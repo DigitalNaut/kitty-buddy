9ace81f0411d37deaa0339bcb9a96445a72b4e2b
DigitalNaut <johnbernalfsd@gmail.com>
2021-07-27 22:34:21 -0500

    Fixed dark mode toggle
    + Added helper class to keep track of adding and removing dark theme toggle

--- src/js/darkThemeToggle.mjs
+++ src/js/darkThemeToggle.mjs
@@ -1,6 +1,9 @@
 window.toggleDarkMode = () => {
-  console.log("Dark theme click");
-  for (let element of document.getElementsByClassName("dark")) {
-    element.classList.toggle("dark");
+
+  const darkElements = document.getElementsByClassName("dark");
+
+  for (let element of darkElements) {
+    if (element.classList.toggle("dark-theme"))
+      element.classList.add("dark-theme");
   }
 };
