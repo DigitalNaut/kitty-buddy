f137a1566e0758c1285ecb1666aae49483b38168
DigitalNaut <johnbernalfsd@gmail.com>
2021-07-28 21:30:29 -0500

    Revamped Footer component
    + Added a modal describing the project
    + Added links
    + Added logos to external websites

--- src/components/Footer/footer.mjs
+++ src/components/Footer/footer.mjs
@@ -11,6 +11,13 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.modal = this.shadowRoot.getElementById("modal");
+    this.modalOpen = this.shadowRoot.getElementById("modal-open");
+    this.modalClose = this.shadowRoot.getElementById("modal-close");
+
+    this.modalOpen.onclick = () => (this.modal.style.display = "block");
+    this.modalClose.onclick = () => (this.modal.style.display = "none");
   }
 }
 
